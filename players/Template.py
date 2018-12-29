71b8daaea5e7fb1766e8bd16ce10b6b7c48b07ab
FelixMo42 <felix@tacocat.com>
2018-12-28 18:55:17 -0800

    change inputs

--- players/Template.py
+++ players/Template.py
@@ -3,11 +3,11 @@ from player import Player
 class Template(Player):
     name = "template"
 
-    def play(self, history, players, totalDice, numDice, dice, prevPlayer):
+    def play(self, history, players, totalDice, numDice, dice):
         prevNum = history[-1, 0]
         prevDie = history[-1, 1]
 
         return [prevNum + 1, prevDie]
 
-    def verify(self, history, players, totalDice, numDice, dice, prevPlayer):
+    def verify(self, history, players, totalDice, numDice, dice):
         return history[-1][0] / totalDice < .5
