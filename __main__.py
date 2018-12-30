0e928b6e2cdfb0ed02c3c4dc43449998b03d0dac
Dean moses <moses@tacocat.com>
2018-12-29 22:09:24 -0500

    p3

--- __main__.py
+++ __main__.py
@@ -4,7 +4,6 @@ from players.Template import Template
 #from players.MiloRoll import MiloRoll
 from players.Felix import Felix, Felix0
 from players.Papa import Papa
-from players.Papa2 import Papa2
 
 players = [
     Template,
