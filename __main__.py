7268a6442ceda4ad05656e40c0f8d1ae5c7f6395
Dean moses <moses@tacocat.com>
2018-12-29 21:37:03 -0500

    cleanup of Papa

--- __main__.py
+++ __main__.py
@@ -1,13 +1,13 @@
 from game.game import Game
 
 from players.Template import Template
-from players.MiloRoll import MiloRoll
+#from players.MiloRoll import MiloRoll
 from players.Felix import Felix, Felix0
 from players.Papa import Papa
 
 players = [
     Template,
-    MiloRoll,
+#    MiloRoll,
     Felix,
     Felix0,
     Papa
--- players/Papa.py
+++ players/Papa.py
@@ -15,15 +15,8 @@ class Papa(Player):
         lastBetQty = lastBet[0]
         lastBetDie = lastBet[1] + 1
 
-        #print(f"Last bet: {lastBet}")
-        print(f"Last bet qty: {lastBetQty}")
-        #print(f"Last bet die: {lastBetDie}")
-        print(f"Total dice: {input.getTotalDice()}")
-
         # accept bet if quantity is less than 1/3 of total quantity of dice
-        response = (lastBetQty * 3 < input.getTotalDice())
-        responseStr = "likely" if response else "unlikely"
-        print(f"Papa thinks last bet of {lastBetQty} dice is {responseStr} given that there's {input.getTotalDice()} total dice")
-        return response
-        #return atleast(input.getBetHistory()[-1, 0], input.getTotalDice()) < .75
-        #return False
+        acceptBet = (lastBetQty * 3 < input.getTotalDice())
+        likelyOrNot = "likely" if acceptBet else "unlikely"
+        print(f"Papa thinks last bet of {lastBetQty} dice is {likelyOrNot} given that there's {input.getTotalDice()} total dice")
+        return acceptBet
